c661073e55a9fd85396c7382f75e6336f382975e
Leonel Santiago Rosas <lsantiago@ittiva.mx>
2024-09-10 22:53:48 -0600

    speed

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import { SpeedInsights } from '@vercel/speed-insights/next';
+import { injectSpeedInsights } from '@vercel/speed-insights';
+
+injectSpeedInsights();
 bootstrapApplication(AppComponent, appConfig).catch((err) =>
   console.error(err)
 );
